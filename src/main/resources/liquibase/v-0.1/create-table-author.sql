e3b72c95ee6d3097e2c6dcda6f9b4d628c860d25
dsoky <dsoky@softserveinc.com>
2017-04-03 14:26:43 +0300

    merged master & develop

--- src/main/resources/liquibase/v-0.1/create-table-author.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE Author
-(
-    author_id integer NOT NULL,
-    name character varying(200) NOT NULL,
-    birth_date date,
-    CONSTRAINT Author_pkey PRIMARY KEY (author_id)
-);
-ALTER TABLE Author
-    OWNER to postgres;
